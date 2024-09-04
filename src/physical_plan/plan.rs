99ed6a35ea600300a963fcc5711adf008ad48772
Balagopal Kanattil <balukanattil@gmail.com>
2024-09-04 09:15:57 -0400

    Complete tree traversal in physical-planner.
    Traversal is done leaf-up so that a physical node has all children
    converted before its conversion. Conversion of single logical node
    to physical node is pending.

--- src/physical_plan/plan.rs
+++ src/physical_plan/plan.rs
@@ -1,4 +1,6 @@
-use std::result;
+use std::{result, sync::Arc};
+
+use crate::logical_plan::LogicalPlan;
 
 pub type Result<T> = result::Result<T, ExecutionError>;
 
@@ -29,6 +31,22 @@ impl ExecutionPlan for FilterExec {
 pub struct ProjectionExec {}
 
 impl ExecutionPlan for ProjectionExec {
+    fn execute(&self) -> Result<RecordBatch> {
+        Ok(RecordBatch {})
+    }
+}
+
+pub struct DummyExec {
+    logical_plan: Arc<LogicalPlan>,
+}
+
+impl DummyExec {
+    pub fn new(logical_plan: Arc<LogicalPlan>) -> Self {
+        Self { logical_plan }
+    }
+}
+
+impl ExecutionPlan for DummyExec {
     fn execute(&self) -> Result<RecordBatch> {
         Ok(RecordBatch {})
     }
--- src/physical_plan/planner.rs
+++ src/physical_plan/planner.rs
@@ -1,8 +1,8 @@
-use std::sync::Arc;
+use std::{cell::RefCell, rc::Rc, sync::Arc};
 
 use crate::logical_plan::LogicalPlan;
 
-use super::plan::ExecutionPlan;
+use super::plan::{DummyExec, ExecutionPlan};
 
 pub type Result<T> = std::result::Result<T, PlannerError>;
 
@@ -16,33 +16,86 @@ impl PhysicalPlanner {
     pub fn create_physical_plan(
         &self,
         logical_plan: Arc<LogicalPlan>,
-    ) -> Result<()> {
+    ) -> Result<Arc<dyn ExecutionPlan>> {
         // For creating a physical node, we need to know all the children nodes first.
         // This means we need a bottom-up traversal of the logical_plan tree.
 
+        let root = physical_node_ref(logical_plan, None);
+
         // Do a BFS to flatten the tree and then walk the tree in reverse order.
-        let root = logical_plan;
         let mut stack = vec![root];
         let mut peek = 0;
         while peek < stack.len() {
-            let node = &stack[peek];
-            for child in node.inputs() {
-                stack.push(child);
+            let node = stack[peek].clone();
+            for child in node.borrow().logical_plan.inputs() {
+                let node = physical_node_ref(child, Some(node.clone()));
+                stack.push(node);
             }
             peek += 1;
         }
 
-        Ok(())
+        let mut root_plan = None;
+
+        // Walk the tree from leaf up.
+        for node in stack.into_iter().rev() {
+            // We expect all children to be converted to physical plan.
+            // TODO: Throw proper error.
+            assert_eq!(
+                node.borrow().children_expected,
+                node.borrow().children_converted.len()
+            );
+
+            // convert the plan.
+            let physical_plan = convert_to_plan(node.clone());
+
+            // update parent.
+            if let Some(ref parent) = node.borrow().parent {
+                parent.borrow_mut().children_converted.push(physical_plan);
+            } else {
+                root_plan = Some(physical_plan);
+            }
+        }
+
+        root_plan.map_or(Err(PlannerError {}), |plan| Ok(plan))
     }
 }
 
-pub enum PlannerError {}
+/// An intermediate holder for generating execution plan.
+struct PhysicalNode {
+    pub children_expected: usize,
+    pub children_converted: Vec<Arc<dyn ExecutionPlan>>,
+    pub parent: Option<PhysicalNodeRef>,
+    pub logical_plan: Arc<LogicalPlan>,
+}
+
+// We only use single thread now.
+// Move to Arc<Mutex<>> if multi-threaded in future.
+type PhysicalNodeRef = Rc<RefCell<PhysicalNode>>;
+
+fn physical_node_ref(
+    logical_plan: Arc<LogicalPlan>,
+    parent: Option<PhysicalNodeRef>,
+) -> PhysicalNodeRef {
+    Rc::new(RefCell::new(PhysicalNode {
+        children_expected: logical_plan.inputs().len(),
+        children_converted: vec![],
+        parent,
+        logical_plan,
+    }))
+}
+
+// TODO: Actual conversion logic.
+fn convert_to_plan(node: PhysicalNodeRef) -> Arc<dyn ExecutionPlan> {
+    Arc::new(DummyExec::new(node.borrow().logical_plan.clone()))
+}
+
+pub struct PlannerError {}
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::prelude::*;
     use crate::logical_plan::helpers::*;
+    use crate::prelude::*;
 
     #[test]
     fn test_create_physical_plan() {
